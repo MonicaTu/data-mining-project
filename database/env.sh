9211ef3a00e4cd4ef3225e972fe4a25429614074
vlsicad <monica.service@gmail.com>
2014-01-12 21:36:24 +0800

    (env.sh) use weka-3-6-10

--- database/env.sh
+++ database/env.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-export CLASSPATH=$CLASSPATH:~/weka-3-6-10/weka.jar:/host/DataMiningProject/weka-3-7-10/weka.jar
+export CLASSPATH=$CLASSPATH:~/weka-3-6-10/weka.jar:/host/DataMiningProject/weka-3-6-10/weka.jar
