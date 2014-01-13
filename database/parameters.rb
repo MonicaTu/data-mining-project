f885e91374d61aa8ef4d8bf083f03dfec6ea3c23
Monica Tu <monica.service@gmail.com>
2014-01-13 19:03:35 +0800

    (parameters) initial.

---
+++ database/parameters.rb
@@ -0,0 +1,5 @@
+#/usr/bin/env ruby
+
+@concept_num = 94
+@features = ['AutoColorCorrelogram', 'CEDD', 'ColorLayout', 'EdgeHistogram', 'FCTH', 'Gabor', 'JCD', 'JpegCH', 'ScalableColor', 'Tamura']
+@features_num = [256, 144, 120, 80, 192, 60, 168, 192, 64, 18]
